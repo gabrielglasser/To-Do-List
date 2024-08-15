8b1f41cc03bf42842f6aa4e75cd16fd54f4477f2
gabrielglasser <sga91000@gmail.com>
2024-08-15 15:17:19 -0300

    botao de apagar e enter funcionando

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const newTask = document.querySelector('.newTask');
 const btnNewTask = document.querySelector('.btnNewTask');
 const task = document.querySelector('.tasks');
 
-function createList(){
+function createList(){ //cria lista
     const li = document.createElement('li');
     return li;
 }
@@ -17,17 +17,35 @@ newTask.addEventListener('keypress', function(e) {  //funcao para quando precion
 function clearInput() {   //funcao para limpar o input 
     newTask.value = '';
     newTask.focus();
-}
+};
+
+function createBtnClean(li) { // funcao para criar botao de limpar
+    li.innerHTML += '  ';
+    const btnClear = document.createElement('button');
+    btnClear.innerText = 'Apagar';
+    btnClear.setAttribute('class', 'clear');
+    li.appendChild(btnClear);
+};
 
 
-function createTask(textoInput){
+function createTask(textoInput){  //funcao para cliar a lista de tarefas
     const li = createList();
     li.innerHTML = textoInput;
     task.appendChild(li);
     clearInput();
+    createBtnClean(li);
 };
 
 btnNewTask.addEventListener('click', function(e) {
     if (!newTask.value) return;
     createTask(newTask.value);
+});
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+
+    if (el.classList.contains('clear')) {
+        el.parentElement.remove();
+    }
+
 });
